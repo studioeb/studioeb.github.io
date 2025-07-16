259048dee5de8099db7ff425fa02aa2f5fbd2848
Ethan Bayer <studioethanb@gmail.com>
2025-07-15 22:03:47 -0400

    Added basic menu functionality + styling
    Still to-do:
    Make the sidebar hide when other buttons are clicked
    Add a fun menu icon transition to X
    Add aria-label switch
    Fix JS/CSS bugs
    Make transition only occur when the menu button is pressed

---
+++ scripts/menu-button.js
@@ -0,0 +1,42 @@
+/*
+HTML source for Ethan Bayer's personal website
+Copyright (C) 2025 Ethan Bayer
+
+This file is part of the source code for studioeb.github.io.
+
+The source code of studioeb.github.io is free software: you can
+redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+The source code of studioeb.github.io is distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+let menuOpened = false;
+const sidebar = document.getElementById("sidebar");
+
+function openCloseMenu() {
+  if (menuOpened) {
+    closeMenu();
+    menuOpened = false;
+  } else if (!menuOpened) {
+    openMenu();
+    menuOpened = true;
+  }
+}
+
+function openMenu() {
+  sidebar.style.marginLeft = "0";
+}
+
+function closeMenu() {
+  sidebar.style.marginLeft =
+    "-" +
+    window.getComputedStyle(document.body).getPropertyValue("--sidebar-width");
+}
